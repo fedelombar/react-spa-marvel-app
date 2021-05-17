dbd71ec54339cb255ed74bc57a6434356c8aa1e7
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-05-17 16:54:57 -0300

    first commit

---
+++ src/HeroesApp.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useReducer } from "react";
+import { AuthContext } from "./auth/AuthContext";
+import { authReducer } from "./auth/authReducer";
+import { AppRouter } from "./routers/AppRouter";
+
+const init = () => {
+  return (
+    JSON.parse(localStorage.getItem("user")) || {
+      logged: false,
+    }
+  );
+};
+
+export const HeroesApp = () => {
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+
+  //keep the name in the navbar
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
+  return (
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <AppRouter />
+    </AuthContext.Provider>
+  );
+};
